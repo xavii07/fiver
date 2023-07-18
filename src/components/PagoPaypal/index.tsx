16f6cfd3d97c700435e2aada77b4e57cad5934bd
xavii07 <xaviergaspa123@gmail.com>
2023-07-18 11:10:09 -0500

    feat pago reserva

--- src/components/PagoPaypal/index.tsx
+++ src/components/PagoPaypal/index.tsx
@@ -2,9 +2,14 @@ import { Container } from "@mui/material";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 const PagoPaypayComponent: React.FC = () => {
+  const options = {
+    clientId:
+      "AXmFalf1lhKeWU7JY3bsLnWROGHsFqnLhW8_VPEYBPVS5Vy4L1vrKCAmfyGCEdkHbrBvQTqxgMbFEcSc",
+  };
+
   return (
     <Container maxWidth="lg" style={{ marginTop: "3rem" }}>
-      <PayPalScriptProvider>
+      <PayPalScriptProvider options={options}>
         <PayPalButtons />
       </PayPalScriptProvider>
     </Container>
